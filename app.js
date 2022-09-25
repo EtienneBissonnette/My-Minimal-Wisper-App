745b6da46e21f8836944d7d86fa9a13c4cb3eabc
Etienne <bissonnette@live.ca>
2022-09-25 16:01:55 -0400

    Configured server app

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+require('dotenv').config();
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`)
+});
